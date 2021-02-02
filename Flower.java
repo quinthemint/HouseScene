f73f0a7676651565a85ab323708a27b459ed5575
quinthemint <59623248+quinthemint@users.noreply.github.com>
2021-02-01 18:56:48 -0800

    Created drawable flower class

--- Flower.java
+++ Flower.java
@@ -1,3 +1,4 @@
+import processing.core.*;
 class Flower implements Drawable {
  
  PApplet p; 
@@ -8,10 +9,16 @@ class Flower implements Drawable {
  
 public void draw(int x, int y){
    p.pushMatrix();
+     p.fill(0,0,0);
+     p.strokeWeight(5);
+     p.line(x,y,x,y+100);
+     p.strokeWeight(0);
      p.fill(50, 20, 100);
-     p.ellipse(x,y,w,h);
-     p.ellipse(x,y,w,h);
-     p.circle();
+     p.ellipse(x,y,100,50);
+     p.ellipse(x,y,50,100);
+     p.ellipse(x,y,75,75);
+     p.fill(180,180,180);
+     p.circle(x,y,25);
    p.popMatrix();
  }
 }
--- HouseScene.pde
+++ HouseScene.pde
@@ -1,8 +1,10 @@
+Flower flower;
 void setup(){
   size(800, 600);
+  flower = new Flower(this);
 }
-
 
 void draw(){
   background(45, 211, 250);
+  flower.draw(200,500);
 }
